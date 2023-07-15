a317ed0b57044c18c584be41e773c67f1284ab74
Yan Gonçalves <yanw100@gmail.com>
2023-07-15 15:40:55 -0300

    Iniciando projeto

---
+++ database/migrations/2023_07_15_183013_create_lists_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::connection('mongodb')->create('lists', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::connection('mongodb')->dropIfExists('lists');
+    }
+};
