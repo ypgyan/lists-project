a9aa63c4f18550a6b0272ff751d357fa5bb84d1d
Yan Gonçalves <yanw100@gmail.com>
2023-07-16 15:46:43 -0300

    feature: creating model for table

---
+++ app/Models/Mongo/TodoList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Mongo;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class TodoList extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mongodb';
+}
