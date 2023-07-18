35dcc16e171c2b2f8245ec6ea11750804c96f19c
Yan Gonçalves <yanw100@gmail.com>
2023-07-18 20:12:19 -0300

    feature: Custom response for not found and server error.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,8 @@
 
 namespace App\Exceptions;
 
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -41,10 +43,28 @@ class Handler extends ExceptionHandler
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if (env('APP_ENV') == 'production') {
+            if ($e instanceof Exception) {
+                return response()->json([
+                    'message' => 'Sorry, something went wrong :('
+                ], 500);
+            }
+        }
+        if ($e instanceof ModelNotFoundException) {
+            return response()->json([
+                'message' => 'Resource not found'
+            ], 404);
+        }
+
+        return parent::render($request, $e);
     }
 }
