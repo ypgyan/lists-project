b83d4a4163a4e159265b7b4a566fe08c382668c2
Yan Gonçalves <yanw100@gmail.com>
2023-07-18 19:35:19 -0300

    refactor: Changed request name to a more generic name to be used on update too.

--- app/Http/Requests/Api/Todos/TodoFormRequest.php
+++ app/Http/Requests/Api/Todos/TodoFormRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Api\Todos;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class TodoCreateRequest extends FormRequest
+class TodoFormRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
